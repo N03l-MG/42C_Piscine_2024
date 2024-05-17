04993f513dcc5b0b75bff7e677617e93b4655403
N03l-MG <noelmonzongarcia@gmail.com>
2024-05-17 15:05:23 +0200

    c00 ex05 and cleanup

---
+++ Projects/c00/ex05/ft_print_comb.c
@@ -0,0 +1,47 @@
+// Post-Piscine work for personal fullfilment! //
+
+#include <unistd.h>
+
+void	write_if_unique(char a, char b, char c)
+{
+	if (!(a == b && b == c && c == a)) {
+		write(1, &c, 1);
+		write(1, &b, 1);
+		write(1, &a, 1);
+		if (!(c == '7' && b == '8' && a == '9')) {
+			write(1, ", ", 2);
+		}
+	}
+}
+
+void	ft_print_comb(void)
+{
+	char a;
+	char b;
+	char c;
+
+	c = '0';
+	while (c <= '7')
+	{
+		b = c + 1;
+		while (b <= '8')
+		{
+			a = b + 1;
+			while (a <= '9')
+			{
+				write_if_unique(a, b, c);
+				a++;
+			}
+			a = '0';
+			b++;
+		}
+		b = '0';
+		c++;
+	}
+}
+
+// int	main()
+// {
+// 	ft_print_comb();
+// 	return 0;
+// }
